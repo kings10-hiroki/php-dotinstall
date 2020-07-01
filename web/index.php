28d9357f3d895517f406506e271edcd5410c1a20
kings10-hiroki <h61147366@gmail.com>
2020-07-01 18:00:11 +0900

    complete lecture

--- web/index.php
+++ web/index.php
@@ -2,14 +2,39 @@
 
 require('./app/functions.php');
 
+createToken();
+
+define('FILENAME', './app/message.txt');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  validateToken();
+  $message = trim(filter_input(INPUT_POST, 'message'));
+  $message = $message !== '' ? $message : '...';
+
+  $fp = fopen(FILENAME, 'a');
+  fwrite($fp, $message . "\n");
+  fclose($fp);
+
+  header('Location: http://localhost:8080/result.php');
+  exit;
+}
+
+$messages = file(FILENAME, FILE_IGNORE_NEW_LINES);
+
 include('./app/_header.php');
 
 ?>
 
-<form action="result.php" method="get">
+<ul>
+  <?php foreach ($messages as $message) : ?>
+    <li><?= h($message); ?></li>
+  <?php endforeach; ?>
+</ul>
+
+<form action="" method="post">
   <input type="text" name="message">
-  <input type="text" name="username">
-  <button>send</button>
+  <button>post</button>
+  <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
 </form>
 
 
--- web/result.php
+++ web/result.php
@@ -2,16 +2,15 @@
 
 require('./app/functions.php');
 
-$message = filter_input(INPUT_GET, 'message');
-$username = filter_input(INPUT_GET, 'username');
-
 include('./app/_header.php');
 
 ?>
 
-<p><?= h($message); ?> by <?= h($username); ?></p>
+<p>message added!</p>
 <p><a href="index.php">Go Back</a></p>
 
 <?php
 
 include('./app/_footer.php');
+
+?>
